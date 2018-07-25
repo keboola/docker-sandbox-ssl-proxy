3b8f5cbf90d237c0100f3ef3de24432db241c958
Odin <odinuv@gmail.com>
2018-07-25 12:07:06 +0200

    fix: email is deprecated

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ pip install --user awscli
 # put aws in the path
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker tag keboola/docker-sandbox-ssl-proxy:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
 docker tag keboola/docker-sandbox-ssl-proxy:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:latest
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
