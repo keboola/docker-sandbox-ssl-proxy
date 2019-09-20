6a309b45e44ee29ae67887764a02ad471e0d26ff
pivnicek <marc@keboola.com>
2019-09-20 13:05:06 +0200

    switch deployment to devel ecr

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars 
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
-docker tag keboola/docker-sandbox-ssl-proxy:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
-docker tag keboola/docker-sandbox-ssl-proxy:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:latest
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
-docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:latest
+docker tag keboola/docker-sandbox-ssl-proxy:latest 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
+docker tag keboola/docker-sandbox-ssl-proxy:latest 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:latest
+docker push 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:$TRAVIS_TAG
+docker push 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/docker-sandbox-ssl-proxy:latest
